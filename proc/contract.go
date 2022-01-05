e8335e821a5ede15b9600a9ef8ca4a7169ea10b5
Yueming Xu <yxu@tibco.com>
2022-01-05 00:14:15 -0700

    debug

--- proc/contract.go
+++ proc/contract.go
@@ -310,6 +310,7 @@ func DecodeTransactionInput(input []byte, address string, blockTime int64) (*Dec
 
 	if glog.V(2) {
 		glog.Infof("decode contract %s method %s tx data %s", address, methodID, hex.EncodeToString(input))
+		glog.Flush()
 	}
 	data, err := abi.Decode(method.Inputs, input[4:])
 	if err != nil {
