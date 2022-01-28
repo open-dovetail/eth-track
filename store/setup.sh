040b17ddbda9ed91109a4a8558101abecc0ddf02
Yueming Xu <yxu@tibco.com>
2022-01-28 10:06:14 -0700

    add aggregated view

--- store/setup.sh
+++ store/setup.sh
@@ -1,4 +1,7 @@
 WORK=~/work/clickhouse
 cp tables.sql $WORK/ethdb.sql
+cp aggregated_views.sql $WORK/aggregated_views.sql
+
 cd $WORK
 ./clickhouse client --password clickhouse --multiquery < ethdb.sql
+./clickhouse client --password clickhouse --multiquery < aggregated_views.sql
